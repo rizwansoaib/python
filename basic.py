f55a4f8364ae47bcf5254a0c7b51e0c60b118307
RIZWAN AHMAD <rizwansoaib@gmail.com>
2018-07-19 11:11:02 +0530

    python basic

---
+++ basic.py
@@ -0,0 +1,16 @@
+'''Hello welcome to my python repo on github. I tell you basic programme in python '''
+
+#print function in python
+
+print("Text goes here")
+
+
+#take input from user as string and stored in variable a 
+
+a=input() 
+
+#take input as integer first take as string the typecast into integer
+
+a=int(input())
+
+
